3bbac002db5776cdf559f9727fde73a64487f8f0
André Monello <atmonello@outlook.com>
2020-03-29 00:31:08 -0300

    Reverting to default Vuetify dark theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,22 +58,19 @@ module.exports = {
   vuetify: {
     customVariables: [],
     theme: {
-      disable: true
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
     }
-    // theme: {
-    //   dark: true,
-    //   themes: {
-    //     dark: {
-    //       primary: colors.blue.darken2,
-    //       accent: colors.grey.darken3,
-    //       secondary: colors.amber.darken3,
-    //       info: colors.teal.lighten1,
-    //       warning: colors.amber.base,
-    //       error: colors.deepOrange.accent4,
-    //       success: colors.green.accent3
-    //     }
-    //   }
-    // }
   }
   // /*
   //  ** Build configuration
