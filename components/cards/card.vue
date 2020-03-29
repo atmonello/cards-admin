e3612309a32f6013cdd6085cf411b56dcc001c8e
André Monello <atmonello@outlook.com>
2020-03-28 23:55:20 -0300

    Creating cards and cards container components

---
+++ components/cards/card.vue
@@ -0,0 +1,37 @@
+<template>
+  <li :class="{ dark: card.color == 'black', light: card.color == 'white' }">
+    <h3>{{ card.content }}</h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+li {
+  display: block;
+  height: 300px;
+  width: 240px;
+  box-shadow: 2px 3px 0px 1px rgba(0, 0, 0, 0.25);
+  box-sizing: border-box;
+  padding: 0.8rem;
+
+  &.dark {
+    background-color: #000;
+    color: #fff;
+  }
+
+  &.light {
+    background-color: #fff;
+    color: #000;
+  }
+}
+</style>
---
+++ components/cards/container.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="mnl-cards__cards-container">
+    <card
+      v-for="card in cards"
+      :key="card.id"
+      class="mnl-cards__cards-container__card"
+      :card="card"
+    />
+  </ul>
+</template>
+
+<script>
+import Card from "./card";
+
+export default {
+  components: {
+    Card
+  },
+  props: {
+    cards: {
+      required: true,
+      type: Array
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.mnl-cards {
+  &__cards {
+    &-container {
+      list-style: none;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+
+      li {
+        flex-shrink: 0;
+        margin: 0.8rem;
+      }
+    }
+  }
+}
+</style>
